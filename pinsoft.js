c802cb990986f20770930e6e4e566fb4f5f1d056
Hüseyin <87951691+Huseyinoney@users.noreply.github.com>
2022-10-21 02:33:18 +0300

    Add files via upload

--- pinsoft.js
+++ pinsoft.js
@@ -1,6 +1,6 @@
 
 function Minesweeper(square) {
- let array1 = []
+ 
     for (let i=0 ; i<square.length; i++) {
         for(let j =0; j<square[0].length; j++) {
             
@@ -8,17 +8,18 @@ function Minesweeper(square) {
 
                 for(let a = i - 1 ; a<=i+1 ; a++){
 
-                    if(a<0 || a>square.length) continue
+                    if(a<0 || a>=square.length) continue
 
                     for(let b = j - 1 ; b<=j+1 ; b++){
 
-                        if(b<0 || b>square[0].length) continue
+                        if(b<0 || b>=square[0].length) continue
 
                         else if (square[a][b]==="*") continue
 
                         else 
                         {
-                            
+                            let variable =   Number(square[a][b]) + 1
+                            square[a][b] =  String(variable)  
                             //ipucu sayılarını yeniden düzenleme kısmı
 
                         }
@@ -31,7 +32,13 @@ function Minesweeper(square) {
         }
         
     }
-    return array1
+    return square
 }
-let square = ["*000","0000","0*00","0000"]
-console.log(Minesweeper(square))
+let square = [["*","0","0","0"],["0","0","0","0"],["0","*","0","0"],["0","0","0","0"]]
+ let a = Minesweeper(square)
+
+ function show() {
+ for(let i =0; i<square.length;i++){
+    console.log(a[i])
+}}
+show()
